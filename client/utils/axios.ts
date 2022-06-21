f107e3efaa823f3d2a4dad69ff251a5702ed2b18
Mahmoud Bekheet <mahmoud.a.bekheet@gmail.com>
2022-06-22 01:01:51 +0200

    Add axios

---
+++ client/utils/axios.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+// ----------------------------------------------------------------------
+
+//* fetched from .env file
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:8080/api',
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) =>
+    Promise.reject(
+      (error.response && error.response.data) || 'Something went wrong'
+    )
+);
+
+export default axiosInstance;
